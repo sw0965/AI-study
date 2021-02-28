e48bf5b0c8f2d5b6b2ed25c3b053617a591a2839
sw0965 <hsw0965@naver.com>
2021-03-01 03:26:19 +0900

    tensorflow_gpu_test

--- tensorflow_gpu_test/tensor-gpu_check.py
+++ tensorflow_gpu_test/tensor-gpu_check.py
@@ -3,4 +3,4 @@ import tensorflow as tf
 print('텐서플로 버전 :', tf.__version__)
 print('cuda로 빌드 되었나? :', tf.test.is_built_with_cuda())
 print('cuda와 같은 gpu로 빌드 되었나? :', tf.test.is_built_with_gpu_support())
-print('사용가능한 gpu 출력 :', tf.test.gpu_device_name())
+print('사용가능한 gpu 출력. :', tf.test.gpu_device_name())
