202c615080ff31694a408084246b57e3f1484faa
sw0965 <hsw0965@naver.com>
2021-03-01 03:25:58 +0900

    Crawling

--- Crawling/crawling.py
+++ Crawling/crawling.py
@@ -11,6 +11,48 @@ driver.get("https://search.shopping.naver.com/search/all?query=%ED%99%94%EC%9E%A
 driver.implicitly_wait(5)
 
 
+# '//*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/ul/div/div['+i+']/li/div/div[2]/div[1]/a'
+# '//*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/ul/div/div['+i+']/li/div[1]/div[2]/div[1]/a'
+src = []
+# title = driver.find_elements_by_css_selector(".basicList_title__3P9Q7")
+# print(len(title))
+for i in map(str, range(1,49)):
+    if i==i:
+        title = driver.find_element_by_xpath('//*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/ul/div/div['+i+']/li/div/div[2]/div[1]/a').click()
+    else:
+        title = driver.find_element_by_xpath('//*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/ul/div/div['+i+']/li/div[1]/div[2]/div[1]/a').click()
+# //*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/ul/div/div[6]/li/div/div[2]/div[1]/a
+
+    time.sleep(1)
+    driver.switch_to.window(driver.window_handles[-1])
+    driver.implicitly_wait(3)
+    time.sleep(1)
+
+    try:
+        imgs = driver.find_element_by_xpath(
+            '//*[@id="__next"]/div/div[2]/div[2]/div[2]/div[1]/div/div[1]/div[1]/div/img').get_attribute("src")
+    except:
+        imgs = driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div[1]/div[1]/div[1]/img').get_attribute(
+            "src")
+        
+
+    src.append(imgs)
+
+    print("포문 횟수:", i)
+    print("이미지 소스:", src)
+    print("길이:", len(src))
+    time.sleep(1)
+
+    driver.close()
+    time.sleep(1)
+
+    driver.switch_to.window(driver.window_handles[0])
+    time.sleep(1)
+
+
+
+
+
 '''
 click_item = driver.find_element_by_xpath('//*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/ul/div/div[1]/li/div/div[2]/div[1]/a').click()
 driver.switch_to.window(driver.window_handles[-1])
@@ -25,75 +67,33 @@ urllib.request.urlretrieve(img_area, "C:/Users/user/Desktop/imgs/test.jpg")     
 driver.close()
 driver.switch_to.window(driver.window_handles[0])
 time.sleep(3)
-'''
-# click_item = driver.find_elements_by_css_selector(".basicList_link__1MaTN")
-src = []
-#원본
-# for i in map(str, range(1, 49)):
-#     try:
-#         click_item = driver.find_element_by_xpath(
-#             '//*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/ul/div/div['+i+']/li/div/div[2]/div[1]/a').click()
-#     except:
-#         click_item = driver.find_element_by_xpath(
-#             '//*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/ul/div/div['+i+']/li/div[1]/div[2]/div[1]/a').click()
-#              //*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/ul/div/div[15]/li/div[1]/div[2]/div[1]/a
-title = driver.find_elements_by_css_selector(".basicList_title__3P9Q7")
-print(len(title))
-for i in title:
-    title[i].click()
-    time.sleep(2)
-    driver.switch_to.window(driver.window_handles[-1])
-    driver.implicitly_wait(5)
-    time.sleep(2)
-
-    try:
-        imgs = driver.find_element_by_xpath(
-            '//*[@id="__next"]/div/div[2]/div[2]/div[2]/div[1]/div/div[1]/div[1]/div/img').get_attribute("src")
-    except:
-        imgs = driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div[1]/div[1]/div[1]/img').get_attribute(
-            "src")
-        pass
-
-    src.append(imgs)
-
-    print("포문 횟수:", i)
-    print("이미지 소스:", src)
-    print("길이:", len(src))
-    time.sleep(1)
-
-
-
-    driver.close()
-    time.sleep(2)
-
-    driver.switch_to.window(driver.window_handles[0])
-    time.sleep(2)
-
-# //*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/ul/div/div[1]/li/div/div[2]/div[1]/a
-# //*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/ul/div/div[2]/li/div/div[2]/div[1]/a
-# //*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/ul/div/div[5]/li/div[1]/div[2]/div[1]/a
-# //*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/ul/div/div[6]/li/div[1]/div[2]/div[1]/a
-# basicList_link__1MaTN
-# basicList_link__1MaTN
 
 
 
 
-'''
-cosmetic_category = range(10)
-img_num = range(48)
-page_num = range(30)
+//*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/ul/div/div[1]/li/div/div[2]/div[1]/a
+//*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/ul/div/div[2]/li/div/div[2]/div[1]/a
+//*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/ul/div/div[3]/li/div/div[2]/div[1]/a
+//*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/ul/div/div[4]/li/div/div[2]/div[1]/a
+//*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/ul/div/div[5]/li/div[1]/div[2]/div[1]/a
+//*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/ul/div/div[6]/li/div/div[2]/div[1]/a
+//*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/ul/div/div[7]/li/div[1]/div[2]/div[1]/a
+//*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/ul/div/div[8]/li/div[1]/div[2]/div[1]/a
+//*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/ul/div/div[9]/li/div[1]/div[2]/div[1]/a
+//*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/ul/div/div[1]/li/div[1]/div[2]/div[1]/a
+//*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/ul/div/div[1]/li/div[1]/div[2]/div[1]/a
+//*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/ul/div/div[1]/li/div/div[2]/div[1]/a
+//*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/ul/div/div[1]/li/div[1]/div[2]/div[1]/a
+//*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/ul/div/div[1]/li/div[1]/div[2]/div[1]/a
+//*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/ul/div/div[5]/li/div/div[2]/div[1]/a
+//*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/ul/div/div[6]/li/div/div[2]/div[1]/a
+//*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/ul/div/div[1]/li/div[1]/div[2]/div[1]/a
+//*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/ul/div/div[8]/li/div[1]/div[2]/div[1]/a
+//*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/ul/div/div[1]/li/div[1]/div[2]/div[1]/a
+//*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/ul/div/div[0]/li/div[1]/div[2]/div[1]/a
+//*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/ul/div/div[1]/li/div[1]/div[2]/div[1]/a
+//*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/ul/div/div[2]/li/div/div[2]/div[1]/a
+//*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/ul/div/div[3]/li/div[1]/div[2]/div[1]/a
 
-# for cate in cosmetic_category:
-#     cate = cate+1
-#     cosmetic_category = driver.find_element_by_xpath(
-#         '//*[@id="__next"]/div/div[2]/div[2]/div[2]/div[1]/div[1]/div[2]/ul/li['+cate+']/a').click()
-#
-#     for page in page_num:
-#         page = page+1
-#         pages = driver.find_element_by_xpath('//*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/div[3]/div/a['+page+']')
-#
-#         for img in img_num:
-#             imgs = driver.find_element_by_xpath('//*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/ul/div/div['+img+']/li/div/div[1]/div/a/img')
 
 '''
