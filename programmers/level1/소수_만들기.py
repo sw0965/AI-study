b1f8fad954484bf8737b8f0f40cbf6e96dcd0470
hsw0965 <zx0965@gmail.com>
2022-02-14 16:54:38 +0900

    2022-02-14 한상우 프로그래머스 문제 풀이

---
+++ programmers/level1/소수_만들기.py
@@ -0,0 +1,40 @@
+# 주어진 숫자 중 3개의 수를 더했을 때 소수가 되는 경우의 개수를 구하려고 합니다.
+# 숫자들이 들어있는 배열 nums가 매개변수로 주어질 때, nums에 있는 숫자들 중 서로 다른 3개를 골라 더했을 때 소수가 되는 경우의 개수를 return 하도록 solution 함수를 완성해주세요.
+
+# nums에 들어있는 숫자의 개수는 3개 이상 50개 이하입니다.
+# nums의 각 원소는 1 이상 1,000 이하의 자연수이며, 중복된 숫자가 들어있지 않습니다.
+
+# [1,2,3,4]	    1
+# [1,2,7,6,4]	4
+
+# [1,2,4]를 이용해서 7을 만들 수 있습니다.
+
+# [1,2,4]를 이용해서 7을 만들 수 있습니다.
+# [1,4,6]을 이용해서 11을 만들 수 있습니다.
+# [2,4,7]을 이용해서 13을 만들 수 있습니다.
+# [4,6,7]을 이용해서 17을 만들 수 있습니다.
+
+from itertools import combinations
+import math
+
+def solution(nums):
+    # answer = -1
+    print(list(combinations(nums, 3)))
+    comb = list(combinations(nums, 3))
+    for i in comb:
+        print('i',i)
+        sum_comb = sum(i)
+        print(sum_comb)
+        # for j in range(2, int(math.sqrt(sum_comb))+1):
+        for j in range(2, sum_comb):
+            if sum_comb % j == 0:
+                return False
+
+        return sum_comb
+
+
+    # return answer
+
+
+nums = [1,2,7,6,4]
+print(solution(nums))
